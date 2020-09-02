a1f721f229eb2e0022dc8cd2b0866e08465cd983
Your Name <you@example.com>
2020-09-02 23:29:11 +0300

    added: sprites get loaded from plist

--- OaksleepCityRacing/Sources/RoadScene.cpp
+++ OaksleepCityRacing/Sources/RoadScene.cpp
@@ -1,41 +1,84 @@
 #include "RoadScene.h"
 
-// SIzx Cats logger defines
+// Six Cats logger defines
 #include "SixCatsLogger.h"
 #include "SixCatsLoggerMacro.h"
 #include <sstream>
 
 using namespace oaksleep_city_racing;
+using namespace std;
 USING_NS_CC;
 
-static const int kRoadTileZOrder = 10;
-static const int kRoadTileLabelZOrder = 11;
+#include <cmath> // round
 
-static const int kCarZOrder = 11;
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+enum ZOrderValues {
+  kTerrainTileZOrder = 10,
+  kRoadTileZOrder = 11,
+  kRoadTileLabelZOrder = 12,
+
+  kCarZOrder = 20
+};
 
 static const int kSingleMoveDistance = 400;
 static const float kSingleMoveInterval = 2.0;
 
+static const struct {
+  string redCar;
+  string road01;
+  string road02;
+  string road03;
+  string terrain;
+} kSpriteFileNames = {
+  .redCar = "road_scene/red_car",
+  .road01 = "road_scene/road_tile_01",
+  .road02 = "road_scene/road_tile_02",
+  .road03 = "road_scene/road_tile_03",
+  .terrain = "road_scene/soil_tile"
+};
+
+static const string kPlistFileName = "road_scene.plist";
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+RoadScene::RoadScene() {
+  // nothing to do here
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+RoadScene::~RoadScene() {
+  unloadSpriteCache();
+}
+
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
 Scene* RoadScene::createScene(std::shared_ptr<SixCatsLogger> inC6) {
-
   RoadScene *pRet = new(std::nothrow) RoadScene();
-  if (pRet == nullptr) {
-    return nullptr;
-  }
 
-  pRet->setLogger(inC6);
+  do {
+    if (pRet == nullptr) {
+      break;
+    }
 
-  if (pRet && pRet->init()) {
-    pRet->autorelease();
-    return pRet;
-  }
-  else {
-    delete pRet;
-    pRet = nullptr;
-    return nullptr;
-  }
+    if (!loadSpriteCache(inC6)) {
+      break;
+    }
+
+    pRet->setLogger(inC6);
+
+    if (pRet->init()) {
+      pRet->autorelease();
+      return pRet; //successful exit
+    }
+
+  }while (false);
+
+  // else {
+  delete pRet;
+  pRet = nullptr;
+  return nullptr;
 }
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
@@ -73,12 +116,46 @@ void RoadScene::doMoveCamera() {
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+void RoadScene::doMoveStaticScreen() {
+  Camera* camera = getDefaultCamera();
+  const Vec3 currentCameraPos = camera->getPosition3D();
+
+  Vec3 newCameraPos = Vec3(currentCameraPos.x, currentCameraPos.y + kSingleMoveDistance,
+                           currentCameraPos.z);
+
+  MoveTo* cmt = MoveTo::create(kSingleMoveInterval, newCameraPos);
+  camera->runAction(cmt);
+
+
+  Vec2 newPos = expectedCarPos;
+  newPos.y = newPos.y + kSingleMoveDistance;
+
+  MoveTo* mt = MoveTo::create(kSingleMoveInterval, newPos);
+  expectedCarPos = newPos;
+
+  CallFunc* cf = CallFunc::create([this]() {
+    C6_D1(c6,"calling car to continue ride\n");
+
+    doMoveCamera();
+
+    doMoveCar();
+  });
+
+  car->runAction(Sequence::create(mt, cf, nullptr));
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
 bool RoadScene::init() {
   if ( !Scene::init() ) {
     return false;
   }
 
-  if (!initBackgroundRoad()) {
+  if (!initTerrain()) {
+    return false;
+  }
+
+  if (!initRoad()) {
     return false;
   }
 
@@ -95,55 +172,9 @@ bool RoadScene::init() {
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
-bool RoadScene::initBackgroundRoad() {
-  const Size currentWindowSize = getContentSize();
-  Sprite* sprite = Sprite::create("road_tile_a.png");
-  if (sprite == nullptr) {
-    C6_C1(c6, "Failed to open 'road_tile_a.png'");
-    return false;
-  }
-
-  const Size spriteSize = sprite->getContentSize();
-  int xPos = currentWindowSize.width/2;
-  int yPos = spriteSize.height/2;
-
-  for (int i = 0; i<100; i++) {
-    Sprite* tsp = Sprite::create("road_tile_a.png");
-
-    tsp->setPosition(Vec2(xPos, yPos));
-
-
-    addChild(tsp, kRoadTileZOrder);
-
-    if ((i%10==0)) {
-      std::ostringstream oss;
-      oss << "L:" << i;
-
-      Label* label = Label::createWithTTF(oss.str(), "fonts/Marker Felt.ttf", 24);
-      if (label == nullptr) {
-        C6_C1(c6, "Failed to open 'fonts/Marker Felt.ttf'");
-      }
-      else {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(currentWindowSize.width/2 - currentWindowSize.width/8, yPos));
-
-        // add the label as a child to this layer
-        this->addChild(label, kRoadTileLabelZOrder);
-      }
-    }
-
-    yPos += spriteSize.height;
-  }
-
-
-  return true;
-}
-
-// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
 bool RoadScene::initCar() {
   const Size currentWindowSize = getContentSize();
-  car = Sprite::create("red_car.png");
+  car = Sprite::createWithSpriteFrameName(kSpriteFileNames.redCar);
   if (car == nullptr) {
     C6_C1(c6, "Failed to open 'red_car.png'");
     return false;
@@ -171,6 +202,100 @@ bool RoadScene::initKeyboardProcessing() {
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+bool RoadScene::initRoad() {
+  const Size currentWindowSize = getContentSize();
+  Sprite* sprite = Sprite::createWithSpriteFrameName(kSpriteFileNames.road01);
+  if (sprite == nullptr) {
+    C6_C2(c6, "Failed to open ", kSpriteFileNames.road01);
+    return false;
+  }
+
+  const Size spriteSize = sprite->getContentSize();
+  int xPos = currentWindowSize.width/2;
+  int yPos = lround(spriteSize.height/2);
+
+  for (int i = 0; i<100; i++) {
+    Sprite* tsp = Sprite::createWithSpriteFrameName(kSpriteFileNames.road01);
+
+    tsp->setPosition(Vec2(xPos, yPos));
+
+
+    addChild(tsp, kRoadTileZOrder);
+
+    if ((i%10==0)) {
+      std::ostringstream oss;
+      oss << "L:" << i;
+
+      Label* label = Label::createWithTTF(oss.str(), "fonts/Marker Felt.ttf", 24);
+      if (label == nullptr) {
+        C6_C1(c6, "Failed to open 'fonts/Marker Felt.ttf'");
+      }
+      else {
+        // position the label on the center of the screen
+        label->setPosition(Vec2(currentWindowSize.width/2 - currentWindowSize.width/8, yPos));
+
+        // add the label as a child to this layer
+        this->addChild(label, kRoadTileLabelZOrder);
+      }
+    }
+
+    yPos += (spriteSize.height -1);
+  }
+
+
+  return true;
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+bool RoadScene::initTerrain() {
+  const Size currentWindowSize = getContentSize();
+  Sprite* sprite = Sprite::createWithSpriteFrameName(kSpriteFileNames.terrain);
+  if (sprite == nullptr) {
+    C6_C2(c6, "Failed to open ", kSpriteFileNames.terrain);
+    return false;
+  }
+
+  const Size spriteSize = sprite->getContentSize();
+  int xPos = currentWindowSize.width - spriteSize.width/2;
+  int yPos = 0;
+
+  for (int i = 0; i<100; i++) {
+    Sprite* tsp = Sprite::createWithSpriteFrameName(kSpriteFileNames.terrain);
+    tsp->setPosition(Vec2(0, yPos));
+    addChild(tsp, kTerrainTileZOrder);
+
+    tsp = Sprite::createWithSpriteFrameName(kSpriteFileNames.terrain);
+    tsp->setPosition(Vec2(xPos, yPos));
+    addChild(tsp, kTerrainTileZOrder);
+
+
+    yPos += (spriteSize.height -1);
+  }
+
+
+  return true;
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+bool RoadScene::loadSpriteCache(std::shared_ptr<SixCatsLogger> c6) {
+  SpriteFrameCache* const sfc = SpriteFrameCache::getInstance();
+
+  sfc->addSpriteFramesWithFile(kPlistFileName);
+  if (!sfc->isSpriteFramesWithFileLoaded(kPlistFileName)) {
+    C6_C2(c6, "Failed to find ", kPlistFileName);
+    return false;
+  }
+
+  // AnimationCache * const ac = AnimationCache::getInstance();
+  // ac->addAnimationsWithFile(kAnimationsPlistFileName);
+
+  return true;
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
 void RoadScene::onKeyPressedScene(EventKeyboard::KeyCode keyCode, Event *) {
   C6_D3(c6, "Key '", (int)keyCode, "' was pressed");
 
@@ -179,7 +304,7 @@ void RoadScene::onKeyPressedScene(EventKeyboard::KeyCode keyCode, Event *) {
     // Director::getInstance()->popScene();
   }
   else if (EventKeyboard::KeyCode::KEY_K == keyCode) {
-    doMoveCar();
+    doMoveStaticScreen();
   }
   else if (EventKeyboard::KeyCode::KEY_X == keyCode) {
     c6->d(__c6_MN__, "Need to get out.");
@@ -187,6 +312,13 @@ void RoadScene::onKeyPressedScene(EventKeyboard::KeyCode keyCode, Event *) {
     // Close the cocos2d-x game scene and quit the application
     Director::getInstance()->end();
   }
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+void RoadScene::unloadSpriteCache() {
+  // ActorNode::unloadAnimations();
+  SpriteFrameCache::getInstance()->removeSpriteFramesFromFile(kPlistFileName);
 }
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
--- OaksleepCityRacing/Sources/RoadScene.h
+++ OaksleepCityRacing/Sources/RoadScene.h
@@ -10,17 +10,25 @@ public:
   static cocos2d::Scene* createScene(std::shared_ptr<SixCatsLogger> c6);
 
 protected:
+  RoadScene();
+  virtual ~RoadScene();
+
   cocos2d::Sprite* car;
   cocos2d::Vec2 expectedCarPos;
 
   void doMoveCar();
   void doMoveCamera();
+  void doMoveStaticScreen();
 
   virtual bool init();
-  bool initBackgroundRoad();
+  bool initRoad();
+  bool initTerrain();
   bool initCar();
   bool initKeyboardProcessing();
   void onKeyPressedScene(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event *);
+
+  static bool loadSpriteCache(std::shared_ptr<SixCatsLogger> c6);
+  static void unloadSpriteCache();
 };
 
 }
